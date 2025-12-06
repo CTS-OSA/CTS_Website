f1c1b83bb3a506868722f96464940520461a3088
pixie1013 <152234323+pixie1013@users.noreply.github.com>
2025-12-07 07:53:30 +0800

    fix: student profile update

--- backend/forms/views/PARDView.py
+++ backend/forms/views/PARDView.py
@@ -335,20 +335,6 @@ class PARDFormView(APIView):
                     {"message": "Appointment updated successfully!"},
                     status=status.HTTP_200_OK
                 )
-            
-            # if pard_instance.status == 'completed':
-                
-            #     PARDSchedule.objects.create(
-            #         student=student, 
-            #         scheduled_date=,
-            #         scheduled_time=
-            #     )
-
-            #     serializer = PARDScheduleSerializer(
-            #         data=request.data, 
-            #         partial=True
-            #     )
-                
         except Exception as e:
             return Response(
                 {"error": f"An error occurred: {str(e)}"},
