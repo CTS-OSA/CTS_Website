e7a8facc6ee2d97b0a0ab28d310bdc200e8e6683
Arthemis11 <presapplesang@gmail.com>
2025-12-08 21:05:05 +0800

    fix: scif pdf photo

--- frontend/src/utils/profileUtils.js
+++ frontend/src/utils/profileUtils.js
@@ -8,3 +8,24 @@ export const getProfileInitials = (profile) => {
   const initials = `${first}${last}`.trim();
   return initials.toUpperCase() || "ID";
 };
+
+export async function toDataUrl(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous"; 
+    img.src = url;
+
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+
+      resolve(canvas.toDataURL("image/png"));
+    };
+
+    img.onerror = reject;
+  });
+}
